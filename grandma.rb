a76d1dcc8a48b4ac790ed0fb1ccab99971ffe975
Jason Markson <jasonmarkson76@gmail.com>
2020-08-19 21:25:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@
 def speak_to_grandma (phrase)
   if phrase == "I LOVE YOU GRANDMA!".upcase
     return "I LOVE YOU TOO PUMPKIN!"
-  end
   elsif phrase == "Hi Nana, how are you?"
     return "HUH?! SPEAK UP, SONNY!"
+  end
 end
