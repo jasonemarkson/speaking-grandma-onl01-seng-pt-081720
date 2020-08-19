04c178ad114e6c1de66759dfb247a07f0f685e9b
Jason Markson <jasonmarkson76@gmail.com>
2020-08-19 21:11:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 phrase = "I LOVE YOU GRANDMA!".upcase
 
 def speak_to_grandma (phrase)
-  if phrase == "I LOVE YOU GRANDMA!".upcase
+  if "I LOVE YOU GRANDMA!".upcase
     returns "I LOVE YOU TOO PUMPKIN!"
   end
 end
